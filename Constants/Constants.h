09d280732232f0b5bec26e1dd8ca6b2a8e69f2dc
abhinayh56 <kumarabhinay53@gmail.com>
2023-02-16 07:39:45 +0530

    added constants

--- Constants/Constants.h
+++ Constants/Constants.h
@@ -1,7 +1,7 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
-#define math_pi 3.141592653589793238
+#define math_pi 3.14159265358979323846
 #define math_e  2.71828182845904523536
 
 #define env_g  9.81
