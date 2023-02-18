fa479bc158cf7f6cd0e44ad530f6aaf2e42bc10f
abhinayh56 <kumarabhinay53@gmail.com>
2023-02-18 20:53:13 +0530

    added main.cpp

---
+++ main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include "Diff_drive_unicycle.h"
+
+using namespace std;
+
+class Complex{
+    public:
+        float real;
+        float imag;
+    private:
+};
+
+Complex sum(Complex c1, Complex c2){
+    Complex c;
+    c.real = c1.real + c2.real;
+    c.imag = c1.imag + c2.imag;
+    return c;
+}
+
+int main(){
+    Diff_drive_unicycle robot1;
+    Diff_drive_unicycle robot2;
+
+    Diff_drive_unicycle robot[10];
+
+    robot1.set_param(0.1, 0.2);
+    robot1.set_v_max(1.0);
+
+    float Vc = 0.2;
+    float Wc = 1.0;
+
+    float wr = 0.0;
+    float wl = 0.0;
+
+    robot1.uni2ddr(Vc,Wc,&wr,&wl);
+
+    // drive_motor(wr,wl);
+
+
+    return 0;
+}
