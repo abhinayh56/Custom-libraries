8eb1d7820450b390d75b92b99fda7af8b87e9faa
abhinayh56 <kumarabhinay53@gmail.com>
2023-02-18 22:25:07 +0530

    commented main.cpp

--- main.cpp
+++ main.cpp
@@ -1,41 +1,41 @@
-#include <iostream>
-#include "Diff_drive_unicycle.h"
+// #include <iostream>
+// #include "Diff_drive_unicycle.h"
 
-using namespace std;
+// using namespace std;
 
-class Complex{
-    public:
-        float real;
-        float imag;
-    private:
-};
+// class Complex{
+//     public:
+//         float real;
+//         float imag;
+//     private:
+// };
 
-Complex sum(Complex c1, Complex c2){
-    Complex c;
-    c.real = c1.real + c2.real;
-    c.imag = c1.imag + c2.imag;
-    return c;
-}
+// Complex sum(Complex c1, Complex c2){
+//     Complex c;
+//     c.real = c1.real + c2.real;
+//     c.imag = c1.imag + c2.imag;
+//     return c;
+// }
 
-int main(){
-    Diff_drive_unicycle robot1;
-    Diff_drive_unicycle robot2;
+// int main(){
+//     Diff_drive_unicycle robot1;
+//     Diff_drive_unicycle robot2;
 
-    Diff_drive_unicycle robot[10];
+//     Diff_drive_unicycle robot[10];
 
-    robot1.set_param(0.1, 0.2);
-    robot1.set_v_max(1.0);
+//     robot1.set_param(0.1, 0.2);
+//     robot1.set_v_max(1.0);
 
-    float Vc = 0.2;
-    float Wc = 1.0;
+//     float Vc = 0.2;
+//     float Wc = 1.0;
 
-    float wr = 0.0;
-    float wl = 0.0;
+//     float wr = 0.0;
+//     float wl = 0.0;
 
-    robot1.uni2ddr(Vc,Wc,&wr,&wl);
+//     robot1.uni2ddr(Vc,Wc,&wr,&wl);
 
-    // drive_motor(wr,wl);
+//     // drive_motor(wr,wl);
 
 
-    return 0;
-}
+//     return 0;
+// }
